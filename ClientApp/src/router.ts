8b17297d526595796189151662d1870898d3000d
Victor Horton <vichorton5@gmail.com>
2024-07-01 11:10:30 -0500

    Add simple show page

--- ClientApp/src/router.ts
+++ ClientApp/src/router.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import RecipeIndex from "./views/recipes/RecipeIndex.vue";
+import RecipeShow from "./views/recipes/RecipeShow.vue";
 
 const routes = [
   {
     path: "/",
     name: "Recipes",
     component: RecipeIndex,
+  },
+  {
+    path: "/:id",
+    name: "Recipe Show",
+    component: RecipeShow,
   },
 ];
 
---
+++ ClientApp/src/views/recipes/RecipeShow.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { recipesData } from "../../data";
+
+const route = useRoute();
+
+const recipe = ref(
+  recipesData.find((recipe) => `${recipe.id}` === route.params.id)
+);
+</script>
+
+<template>
+  <div v-if="recipe">
+    <h1>{{ recipe.name }}</h1>
+    <div v-for="ingredient in recipe.ingredients" :key="ingredient.id">
+      {{ ingredient.name }}
+    </div>
+  </div>
+</template>
