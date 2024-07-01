9d1f8966d26303f4e8da08f391bf117be42c91e0
Victor Horton <vichorton5@gmail.com>
2024-07-01 11:02:59 -0500

    Move files to correct folders

--- ClientApp/src/data.ts
+++ ClientApp/src/data.ts
@@ -2,7 +2,7 @@ import { type Tag } from "./types/tag";
 import { type Ingredient } from "./types/ingredient";
 import { type Recipe } from "./types/recipe";
 
-export const tags: Tag[] = [
+export const tagsData: Tag[] = [
   { id: 1, name: "American" },
   { id: 2, name: "Low Carb" },
   { id: 3, name: "Vegetarian" },
@@ -12,62 +12,72 @@ export const tags: Tag[] = [
   { id: 7, name: "Spicy" },
 ];
 
-export const ingredients: Ingredient[] = [
+export const ingredientsData: Ingredient[] = [
   {
     id: 1,
     name: "Flour",
     description: "All-purpose flour",
-    tags: [tags[3], tags[5]],
+    tags: [tagsData[3], tagsData[5]],
   },
   {
     id: 2,
     name: "Sugar",
     description: "Granulated white sugar",
-    tags: [tags[4]],
+    tags: [tagsData[4]],
   },
-  { id: 3, name: "Eggs", description: "Large eggs", tags: [tags[6]] },
-  { id: 4, name: "Milk", description: "Whole milk", tags: [tags[6], tags[5]] },
+  { id: 3, name: "Eggs", description: "Large eggs", tags: [tagsData[6]] },
+  {
+    id: 4,
+    name: "Milk",
+    description: "Whole milk",
+    tags: [tagsData[6], tagsData[5]],
+  },
   {
     id: 5,
     name: "Chicken",
     description: "Boneless, skinless chicken breasts",
-    tags: [tags[0], tags[2]],
+    tags: [tagsData[0], tagsData[2]],
   },
   { id: 6, name: "Salt", description: "Table salt", tags: [] },
   {
     id: 7,
     name: "Pepper",
     description: "Ground black pepper",
-    tags: [tags[6]],
+    tags: [tagsData[6]],
   },
   {
     id: 8,
     name: "Olive Oil",
     description: "Extra virgin olive oil",
-    tags: [tags[5], tags[2]],
+    tags: [tagsData[5], tagsData[2]],
   },
   {
     id: 9,
     name: "Tomatoes",
     description: "Fresh Roma tomatoes",
-    tags: [tags[5]],
+    tags: [tagsData[5]],
   },
-  { id: 10, name: "Basil", description: "Fresh basil leaves", tags: [tags[5]] },
+  {
+    id: 10,
+    name: "Basil",
+    description: "Fresh basil leaves",
+    tags: [tagsData[5]],
+  },
 ];
 
-export const recipes: Recipe[] = [
+export const recipesData: Recipe[] = [
   {
     id: 1,
     name: "Pancakes",
     description: "Fluffy homemade pancakes",
     ingredients: [
-      ingredients[0],
-      ingredients[2],
-      ingredients[3],
-      ingredients[5],
-      ingredients[6],
+      ingredientsData[0],
+      ingredientsData[2],
+      ingredientsData[3],
+      ingredientsData[5],
+      ingredientsData[6],
     ],
-    tags: [tags[6], tags[0]],
+    tags: [tagsData[6], tagsData[0]],
   },
   {
     id: 2,
@@ -75,51 +85,51 @@ export const recipes: Recipe[] = [
     description:
       "Classic Italian pasta dish with eggs, cheese, pancetta, and pepper",
     ingredients: [
-      ingredients[0],
-      ingredients[2],
-      ingredients[3],
-      ingredients[5],
-      ingredients[6],
-      ingredients[9],
+      ingredientsData[0],
+      ingredientsData[2],
+      ingredientsData[3],
+      ingredientsData[5],
+      ingredientsData[6],
+      ingredientsData[9],
     ],
-    tags: [tags[5]],
+    tags: [tagsData[5]],
   },
   {
     id: 3,
     name: "Chicken Salad",
     description: "Healthy chicken salad with fresh vegetables",
     ingredients: [
-      ingredients[4],
-      ingredients[6],
-      ingredients[7],
-      ingredients[8],
+      ingredientsData[4],
+      ingredientsData[6],
+      ingredientsData[7],
+      ingredientsData[8],
     ],
-    tags: [tags[2], tags[1]],
+    tags: [tagsData[2], tagsData[1]],
   },
   {
     id: 4,
     name: "Chocolate Cake",
     description: "Rich and moist chocolate cake",
     ingredients: [
-      ingredients[0],
-      ingredients[1],
-      ingredients[2],
-      ingredients[3],
-      ingredients[5],
+      ingredientsData[0],
+      ingredientsData[1],
+      ingredientsData[2],
+      ingredientsData[3],
+      ingredientsData[5],
     ],
-    tags: [tags[4]],
+    tags: [tagsData[4]],
   },
   {
     id: 5,
     name: "Tomato Basil Soup",
     description: "Fresh and flavorful tomato basil soup",
     ingredients: [
-      ingredients[8],
-      ingredients[9],
-      ingredients[7],
-      ingredients[5],
-      ingredients[6],
+      ingredientsData[8],
+      ingredientsData[9],
+      ingredientsData[7],
+      ingredientsData[5],
+      ingredientsData[6],
     ],
-    tags: [tags[5], tags[3]],
+    tags: [tagsData[5], tagsData[3]],
   },
 ];
---
+++ ClientApp/src/views/recipes/RecipeIndex.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { recipesData } from "../../data";
+
+const recipes = ref(recipesData);
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <div v-for="recipe in recipes" :key="recipe.id">{{ recipe.name }}</div>
+  </div>
+</template>
