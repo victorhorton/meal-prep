7671191ae0210464e5e309b0c0837d7b3fa4f10a
Victor Horton <vichorton5@gmail.com>
2024-07-25 17:41:51 -0500

    Add SQLite package

--- obj/Debug/net8.0/meal-prep.AssemblyInfo.cs
+++ obj/Debug/net8.0/meal-prep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("meal-prep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020b90184670ba01811470070996ca4be1dfe249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+701d8013c2cd46166708290c6f0204ff0f6c8b9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("meal-prep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("meal-prep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
